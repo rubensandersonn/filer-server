284ff4fe72210e649838d5f20fc7b2cf85666f2d
rubens <rubsandsu@gmail.com>
2019-08-26 11:10:41 -0300

    fixing upload

--- MyFirebase.js
+++ MyFirebase.js
@@ -1,7 +1,17 @@
 const firebase = require("firebase");
 require("dotenv").config();
 
-firebase.initializeApp(process.env.FIREBASE_CONFIG);
+firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 exports.updateRedeAgua = rede => {
   return new Promise(resolve => {
--- upload.js
+++ upload.js
@@ -1,125 +1,191 @@
 const IncomingForm = require("formidable").IncomingForm;
 const MyFirebase = require("./MyFirebase");
 const fs = require("fs");
+const path = require("path");
 
-const fileTypes = ["json", "geojson"];
+const fileTypes = ["json", "json"];
 
 const getExtension = fileName => {
   const s = fileName.split(".");
   return s[s.length - 1];
 };
 
+const isJson = filename => {
+  if (!filename) {
+    return false;
+  }
+  const splitted = filename.split(".");
+
+  if (splitted && splitted[splitted.length - 1] === "json") {
+    return true;
+  }
+  if (splitted && splitted[splitted.length - 1] === "geojson") {
+    return true;
+  }
+  return false;
+};
+
 exports.uploadAgua = function(req, res) {
   var form = new IncomingForm();
 
-  form.on("file", (name, file) => {
-    if (name && fileTypes.indexOf(getExtension(name.toLowerCase())) > -1) {
-      let rawdata = fs.readFileSync(file.path);
-      let rede = JSON.parse(rawdata);
-      // console.log("o que chegou", rede);
-
-      // === salvando arquivo no servidor ===
-      fs.writeFile("/files/" + name, rede, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log("The file ", name, " was saved!");
-      });
-
-      // === salvando dados no firebase ===
-      MyFirebase.updateRedeAgua(rede);
-      // console.log("o que chegou:", type, file.path);
-      // you can access it using file.path
+  form.on("fileBegin", function(name, file) {
+    if (file && isJson(file.name)) {
+      file.name = "rda_meireles.json";
+      file.path = path.join(__dirname, "files", file.name);
+      console.log("(rede agua) comecou a chegar", file.name);
     }
   });
-  form.on("end", () => {
-    res.json();
+
+  form.on("file", (type, file) => {
+    if (file && isJson(file.name)) {
+      console.log("Rede Agua atualizada no servidor:", file.path);
+      let raw = fs.readFileSync(file.path, "utf8");
+      let rede = JSON.parse(raw);
+
+      if (rede) {
+        // === salvando dados no firebase ===
+        MyFirebase.updateRedeGas(rede)
+          .then(res => {
+            console.log("Rede Agua salva  com sucesso no firebase!");
+          })
+          .catch(err => {
+            console.log("ERRO AO SALVAR A REDE AGUA NO FIREBASE!", err);
+          });
+      } else {
+        console.log("nao abriu o arquivo...");
+      }
+    }
   });
+
+  form.on("end", () => {
+    res.send({
+      status: 200
+    });
+  });
+
   form.parse(req);
 };
 
 exports.uploadGas = function(req, res) {
   var form = new IncomingForm();
 
-  form.on("file", (name, file) => {
-    if (name && fileTypes.indexOf(getExtension(name.toLowerCase())) > -1) {
-      let rawdata = fs.readFileSync(file.path);
-      let rede = JSON.parse(rawdata);
-      // console.log("o que chegou", rede);
-
-      // === salvando arquivo no servidor ===
-      fs.writeFile("/files/" + name, rede, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log("The file ", name, " was saved!");
-      });
-      // === salvando dados no firebase ===
-      MyFirebase.updateRedeGas(rede);
+  form.on("fileBegin", function(name, file) {
+    if (file && isJson(file.name)) {
+      file.name = "rdg_meireles.json";
+      file.path = path.join(__dirname, "files", file.name);
+      console.log("(rede gas) comecou a chegar", file.name);
     }
-    // console.log("o que chegou:", type, file.path);
   });
+
+  form.on("file", (type, file) => {
+    if (file && isJson(file.name)) {
+      console.log("Rede gas atualizada no servidor:", file.path);
+      let raw = fs.readFileSync(file.path, "utf8");
+      let rede = JSON.parse(raw);
+
+      if (rede) {
+        console.log("...a rede existe...");
+        // === salvando dados no firebase ===
+        MyFirebase.updateRedeGas(rede)
+          .then(res => {
+            console.log("Rede Gas salva  com sucesso no firebase!");
+          })
+          .catch(err => {
+            console.log("ERRO AO SALVAR A REDE GAS NO FIREBASE!", err);
+          });
+      } else {
+        console.log("nao abriu o arquivo...");
+      }
+    }
+  });
+
   form.on("end", () => {
-    res.json();
+    res.send({
+      status: 200
+    });
   });
+
   form.parse(req);
 };
 
 exports.uploadEsgoto = function(req, res) {
   var form = new IncomingForm();
 
-  form.on("file", (name, file) => {
-    if (name && fileTypes.indexOf(getExtension(name.toLowerCase())) > -1) {
-      let rawdata = fs.readFileSync(file.path);
-      let rede = JSON.parse(rawdata);
-
-      // === salvando arquivo no servidor ===
-      fs.writeFile("/files/" + name, rede, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log("The file ", name, " was saved!");
-      });
-      // === salvando dados no firebase ===
-      MyFirebase.updateRedeEsgoto(rede);
+  form.on("fileBegin", function(name, file) {
+    if (file && isJson(file.name)) {
+      file.name = "rde_meireles.json";
+      file.path = path.join(__dirname, "files", file.name);
+      console.log("(rede esgoto) comecou a chegar", file.name);
     }
-    // console.log("o que chegou:", type, file.path);
-    // you can access it using file.path
   });
+
+  form.on("file", (type, file) => {
+    if (file && isJson(file.name)) {
+      console.log("Rede Esgoto atualizada no servidor:", file.path);
+      let raw = fs.readFileSync(file.path, "utf8");
+      let rede = JSON.parse(raw);
+
+      if (rede) {
+        // === salvando dados no firebase ===
+        MyFirebase.updateRedeGas(rede)
+          .then(res => {
+            console.log("Rede Esgoto salva  com sucesso no firebase!");
+          })
+          .catch(err => {
+            console.log("ERRO AO SALVAR A REDE ESGOTO NO FIREBASE!", err);
+          });
+      } else {
+        console.log("nao abriu o arquivo...");
+      }
+    }
+  });
+
   form.on("end", () => {
-    res.json();
+    res.send({
+      status: 200
+    });
   });
+
   form.parse(req);
 };
 
 exports.uploadViario = function(req, res) {
   var form = new IncomingForm();
 
-  form.on("file", (name, file) => {
-    if (name && fileTypes.indexOf(getExtension(name.toLowerCase())) > -1) {
-      let rawdata = fs.readFileSync(file.path);
-      let rede = JSON.parse(rawdata);
-
-      // === salvando arquivo no servidor ===
-      fs.writeFile("/files/" + name, rede, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log("The file ", name, " was saved!");
-      });
-
-      // === salvando dados no firebase ===
-      MyFirebase.updateRedeViario(rede);
+  form.on("fileBegin", function(name, file) {
+    if (file && isJson(file.name)) {
+      file.name = "rdv_meireles.json";
+      file.path = path.join(__dirname, "files", file.name);
+      console.log("(rede viario) comecou a chegar", file.name);
     }
-    // console.log("o que chegou:", type, file.path);
-    // you can access it using file.path
   });
+
+  form.on("file", (type, file) => {
+    if (file && isJson(file.name)) {
+      console.log("Rede Viario atualizada no servidor:", file.path);
+      let raw = fs.readFileSync(file.path, "utf8");
+      let rede = JSON.parse(raw);
+
+      if (rede) {
+        // === salvando dados no firebase ===
+        MyFirebase.updateRedeGas(rede)
+          .then(res => {
+            console.log("Rede Viario salva  com sucesso no firebase!");
+          })
+          .catch(err => {
+            console.log("ERRO AO SALVAR A REDE ESGOTO NO FIREBASE!", err);
+          });
+      } else {
+        console.log("nao abriu o arquivo...");
+      }
+    }
+  });
+
   form.on("end", () => {
-    res.json();
+    res.send({
+      status: 200
+    });
   });
+
   form.parse(req);
 };
